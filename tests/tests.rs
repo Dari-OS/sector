5ea86ec855530fcc19df0908f9e656582f0147d4
Dari-OS <darius.dev@proton.me>
2025-03-10 22:50:00 +0100

    Added more tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,4 +1,7 @@
-use sector::{states::Normal, Sector};
+use sector::{
+    states::{Manual, Normal},
+    Sector,
+};
 #[test]
 fn test_len() {
     let mut sec = Sector::<Normal, _>::new();
@@ -33,5 +36,41 @@ fn test_len_empty_zst() {
 
 #[test]
 fn test_cap() {
-    let sec = Sector::<Normal, i32>::new();
+    let mut sec = Sector::<Manual, i32>::new();
+    assert_eq!(sec.capacity(), 0);
+    sec.grow(1000);
+    assert_eq!(sec.capacity(), 1000);
+    sec.shrink(1000);
+    assert_eq!(sec.capacity(), 0);
+}
+
+#[test]
+fn test_cap_zst() {
+    let mut sec = Sector::<Manual, ()>::new();
+    assert_eq!(sec.capacity(), usize::MAX);
+    assert_eq!(sec.grow(1000), 0);
+    assert_eq!(sec.capacity(), usize::MAX);
+    assert_eq!(sec.shrink(1000), 0);
+    assert_eq!(sec.capacity(), usize::MAX);
+}
+
+#[test]
+fn test_creation() {
+    let mut sec1 = Sector::<Normal, u32>::new();
+    assert_eq!(sec1.capacity(), 0);
+    assert_eq!(sec1.len(), 0);
+    assert_eq!(sec1.pop(), None);
+
+    let mut sec2 = Sector::<Normal, u32>::with_capacity(100);
+    assert_eq!(sec2.capacity(), 100);
+    assert_eq!(sec2.len(), 0);
+    assert_eq!(sec2.pop(), None);
+
+    let mut sec3 = Sector::<Normal, u32>::try_with_capacity(100).unwrap();
+    assert_eq!(sec3.capacity(), 100);
+    assert_eq!(sec3.len(), 0);
+    assert_eq!(sec3.pop(), None);
+
+    let sec4 = Sector::<Normal, u32>::try_with_capacity(usize::MAX);
+    assert!(sec4.is_err())
 }
